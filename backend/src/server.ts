28dc40315c9a7474cd387adc04bef9e3adfb1c11
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-06-15 04:13:17 +0700

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:3000',
-  process.env.VERCEL_URL
+  'https://purchase-order-management.vercel.app'
 ];
 
 app.use(helmet());
