10a6b151a8b88e30f34e03bad4255c052fb4b194
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-06-15 04:00:37 +0700

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,6 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  process.env.VERCEL_URL
+];
+
 app.use(helmet());
 app.use(cors({
   origin: 'http://localhost:3000',
