1ad681607445379cd085e24de711d534476e53a6
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2025-06-28 23:40:08 +0700

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -75,7 +75,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
